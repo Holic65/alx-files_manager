fac64e19d48e1bf803d708f930ecc2503e0be982
holic65 <okormark@gmail.com>
2023-09-06 10:46:28 +0000

    db.js

---
+++ utils/db.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const {
+      DB_HOST = '127.0.0.1',
+      DB_PORT = 27017,
+      DB_DATABASE = 'files_manager',
+    } = process.env;
+  
+  
+    const url =  `mongodb://${DB_HOST}:${DB_PORT}?directConnection=true`;
+    this.client = MongoClient(url, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+
+    this.client.connect().catch((error) => {
+      console.error('Error occurred while connection to MongoDB:', error);
+    });
+
+    this.db = this.client.db(DB_DATABASE);
+  }
+
+  async isAlive() {
+    return !!this.client && !!this.client.topology && this.client.topology.isConnected(); 
+    //return this.isConnected;
+  }
+  
+  async nbUsers() {
+    const users = this.db.collection('users');
+    const countUsers = await users.countDocuments();
+    return countUsers;
+  }
+
+  async nbFiles() {
+    const files = this.db.collection('files');
+    const countFiles = await files.countDocuments();
+    return countFiles;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
